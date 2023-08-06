55189013a7805e89136992ec24f683d46a97a3be
sagardeswal123 <sagardeswal798@gmail.com>
2023-08-06 20:56:34 +0530

    project started

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import MainRoutes from './components/MainRoutes';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <MainRoutes/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
